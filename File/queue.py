da99dfd02e40f43ee64cca6b16599adeefa87851
blhelias <brieuc.lhelias@gmail.com>
2018-12-17 23:03:55 +0100

    DEV - implement queue (file)

--- File/queue.py
+++ File/queue.py
@@ -1,19 +1,34 @@
-class Queue:
-    def __init__(self):
-        self.in_stack = []
-        self.out_stack = []
+from typing import List
 
-    def __len__(self):
+
+class Queue:
+    """We implement a queue with 2 stacks the 'head' is
+    used to extract elements and the 'queue' to insert elements.
+    """
+    def __init__(self) -> None:
+        self.in_stack: List = [] # queue (stack)
+        self.out_stack: List = [] # head (statck)
+
+    def __len__(self) -> int:
+        """get the length of the queue
+        """
         return len(self.in_stack) + len(self.out_stack)
-    
+
     def push(self, obj):
+        """Add an element in the queue
+        """
         self.in_stack.append(obj)
 
     def pop(self):
+        """remove an element and return it
+        """
         if not self.out_stack:
             self.out_stack = self.in_stack[::-1]
             self.in_stack = []
         return self.out_stack.pop()
 
     def is_empty(self):
-        
+        """Check if the queue is empty
+        """
+        return len(self) == 0
+
--- Test/test_queue.py
+++ Test/test_queue.py
@@ -1,14 +1,23 @@
 from ..File import Queue
+from copy import copy
 
 file = Queue()
 test_list = [1, 2, 3]
 for element in test_list:
     file.push(element)
 
-def test_push():
-
-
 def test_len():
-    assert len(file) == 3
+    file_temp = copy(file)
+    assert len(file_temp) == 3
+    for _ in range(3):
+        file_temp.pop()
+    assert len(file_temp) == 0
+
+def test_is_empty():
+    file_temp = copy(file)
+    assert file.is_empty() == False
+    for _ in range(3):
+        file_temp.pop()
+    assert file_temp.is_empty() == True
 
 
